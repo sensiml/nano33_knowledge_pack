8afb0cae97ed50acb0f7424b425d7d6e91a6bb11
Justin Moore <justin.moore@sensiml.com>
2020-09-17 13:04:35 -0700

    Add initial files for pio project, basic (untested) slide demo for Nano33

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <kb.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  kb_model_init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
