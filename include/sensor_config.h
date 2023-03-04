475e267c4fac1ef0adb8b9903c6811f92711a6f8
Chris Knorowski <chris.knorowski@sensiml.com>
2023-03-03 20:13:26 -0800

    update to hvae serial debug, fix issue where ble was not always getting sent

--- include/sensor_config.h
+++ include/sensor_config.h
@@ -7,7 +7,8 @@
  * BLE Settings
  *
  */
-#define USE_BLE 0
+#define USE_BLE 1
+#define SERIAL_DEBUG 0
 
 typedef struct __attribute__((packed))
 {
@@ -38,20 +39,22 @@ typedef struct __attribute__((packed))
 #define ENABLE_GYRO 1
 #define ENABLE_MAG 0
 
-//IMU ODR settings. Note: Gyroscope and Accel are linked.
-enum {
-    //Sample Rate 0:off, 1:10Hz, 2:50Hz, 3:119Hz, 4:238Hz, 5:476Hz, 6:952Hz
-    ACCEL_GYRO_ODR_OFF = 0,
-    ACCEL_GYRO_ODR_10HZ = 1,
-    ACCEL_GYRO_ODR_50HZ = 2,
+// IMU ODR settings. Note: Gyroscope and Accel are linked.
+enum
+{
+    // Sample Rate 0:off, 1:10Hz, 2:50Hz, 3:119Hz, 4:238Hz, 5:476Hz, 6:952Hz
+    ACCEL_GYRO_ODR_OFF   = 0,
+    ACCEL_GYRO_ODR_10HZ  = 1,
+    ACCEL_GYRO_ODR_50HZ  = 2,
     ACCEL_GYRO_ODR_119HZ = 3,
     ACCEL_GYRO_ODR_238HZ = 4,
     ACCEL_GYRO_ODR_476HZ = 5,
-   // ACCEL_GYRO_ODR_952HZ = 6
+    // ACCEL_GYRO_ODR_952HZ = 6
 };
 
-//Mag ODR settings.
-enum {
+// Mag ODR settings.
+enum
+{
     // range (0..8) = {0.625,1.25,2.5,5,10,20,40,80,400}Hz
     MAG_ODR_0_625HZ = 0,
     MAG_ODR_1_25HZ,
@@ -62,16 +65,16 @@ enum {
     MAG_ODR_40HZ,
     MAG_ODR_80HZ,
     MAG_ODR_400HZ
-} ;
+};
 
-//Default sample rates. Note: Mag will be read at a higher rate.
+// Default sample rates. Note: Mag will be read at a higher rate.
 #define ACCEL_GYRO_DEFAULT_ODR ACCEL_GYRO_ODR_119HZ
 #define MAG_DEFAULT_ODR MAG_ODR_20HZ
 
 #define ENABLE_AUDIO 0
 #if ENABLE_AUDIO
 #define AUDIO_SAMPLE_RATE 16000
-int setup_audio(JsonDocument& config_message, int column_start);
+int      setup_audio(JsonDocument& config_message, int column_start);
 uint8_t* getSampleBuffer();
 #if ENABLE_ACCEL || ENABLE_GYRO || ENABLE_MAG
 #warning "Audio and IMU are enabled. only audio will be used"
@@ -79,13 +82,13 @@ uint8_t* getSampleBuffer();
 #undef ENABLE_GYRO
 #undef ENABLE_MAG
 #define ENABLE_ACCEL 0
-#define ENABLE_GYRO  0
-#define ENABLE_MAG   0
-#endif //#if ENABLE_ACCEL || ENABLE_GYRO || ENABLE_MAG
+#define ENABLE_GYRO 0
+#define ENABLE_MAG 0
+#endif  // #if ENABLE_ACCEL || ENABLE_GYRO || ENABLE_MAG
 
-#endif //ENABLE_AUDIO
+#endif  // ENABLE_AUDIO
 
 void sml_output_results(uint16_t model, uint16_t classification);
-void sml_recognition_run(signed short *data, int num_sensors);
+void sml_recognition_run(signed short* data, int num_sensors);
 
-#endif //__SENSOR_CONFIG_H__
+#endif  //__SENSOR_CONFIG_H__
--- src/main.cpp
+++ src/main.cpp
@@ -37,20 +37,25 @@ static void disconnectedLight()
 
 static void onBLEConnected(BLEDevice central)
 {
+#if SERIAL_DEBUG
     Serial.print("Connected event, central: ");
     Serial.println(central.address());
+#endif  // SERIAL_DEBUG
     connectedLight();
 }
 static void onBLEDisconnected(BLEDevice central)
 {
+#if SERIAL_DEBUG
     Serial.print("Disconnected event, central: ");
     Serial.println(central.address());
+#endif  // SERIAL_DEBUG
     disconnectedLight();
     BLE.setConnectable(true);
 }
 
 void PrintInfo()
 {
+#if SERIAL_DEBUG
     Serial.println("Peripheral advertising info: ");
     Serial.print("Name: ");
     Serial.println(nameOfPeripheral);
@@ -62,17 +67,19 @@ void PrintInfo()
     Serial.println(classOnlyChar.uuid());
     Serial.print("classFeaturesChar UUID: ");
     Serial.println(classFeaturesChar.uuid());
+#endif  // SERIAL_DEBUG
 }
 
 void setup_ble()
 {
     if (!BLE.begin())
     {
+#if SERIAL_DEBUG
         Serial.println("starting BLE failed!");
+#endif  // SERIAL_DEBUG
         while (1)
             ;
     }
-    // Serial.println("BLE Started!");
     BLE.setLocalName(nameOfPeripheral);
     BLE.noDebug();
 
@@ -86,8 +93,10 @@ void setup_ble()
     BLE.setEventHandler(BLEConnected, onBLEConnected);
     BLE.setEventHandler(BLEDisconnected, onBLEDisconnected);
 
+#if SERIAL_DEBUG
     Serial.println("BLE Init done!");
     PrintInfo();
+#endif  // SERIAL_DEBUG
 }
 
 void Send_Notification(uint16_t model_no,
@@ -95,7 +104,6 @@ void Send_Notification(uint16_t model_no,
                        uint8_t* features,
                        uint16_t num_features)
 {
-
     kp_output_t    base_output;
     kp_output_fv_t output_with_features;
     base_output.model          = model_no;
@@ -106,18 +114,23 @@ void Send_Notification(uint16_t model_no,
     {
         output_with_features.fv_len = num_features;
         memcpy(output_with_features.features, features, num_features);
+
         classFeaturesChar.writeValue((void*) &output_with_features, sizeof(kp_output_fv_t));
+#if SERIAL_DEBUG
         Serial.println("Sending With Classification with Features");
+#endif  // SERIAL_DEBUG
     }
     if (classOnlyChar.subscribed())
     {
+#if SERIAL_DEBUG
         Serial.println("Sending Classification");
+#endif  // SERIAL_DEBUG
         classOnlyChar.writeValue((void*) &base_output, sizeof(kp_output_t));
     }
 }
 #endif  // USE_BLE
 
-static unsigned long currentMs, previousMs;
+static unsigned long currentMs, previousMs, previousBLECheckMs;
 static unsigned long interval    = 0;
 int                  num_sensors = ((ENABLE_ACCEL * 3) + (ENABLE_GYRO * 3) + (ENABLE_MAG * 3));
 int                  ret         = 0;
@@ -154,7 +167,9 @@ static void setup_imu()
 {
     if (!IMU.begin())  // Initialize IMU sensor
     {
+#if SERIAL_DEBUG
         Serial.println("Failed to initialize IMU!");
+#endif  // SERIAL_DEBUG
         while (1)
             ;
     }
@@ -176,11 +191,11 @@ static void setup_imu()
     IMU.setAccelODR(ACCEL_GYRO_ODR_OFF);
     IMU.setGyroODR(ACCEL_GYRO_DEFAULT_ODR);
 
-#endif
+#endif  // ENABLE_ACCEL
 
 #if ENABLE_MAG
     IMU.setMagnetODR(mag_speed);
-#endif
+#endif  // ENABLE_MAG
     IMU.setContinuousMode();
 }
 
@@ -217,38 +232,43 @@ static void update_imu()
 
 #if SML_PROFILER
 #define Serial1_OUT_CHARS_MAX 2048
-float recent_fv_times[MAX_VECTOR_SIZE];
+float        recent_fv_times[MAX_VECTOR_SIZE];
 unsigned int recent_fv_cycles[MAX_VECTOR_SIZE];
 #else
 #define Serial1_OUT_CHARS_MAX 512
-#endif
+#endif  // SML_PROFILER
 
 #ifdef __GNUC__
 #pragma GCC diagnostic ignored "-Wunused-function"
-#endif
+#endif  //__GNUC__
 
 
-static char Serial1_out_buf[Serial1_OUT_CHARS_MAX];
-static uint8_t recent_fv[MAX_VECTOR_SIZE];
+static char     Serial1_out_buf[Serial1_OUT_CHARS_MAX];
+static uint8_t  recent_fv[MAX_VECTOR_SIZE];
 static uint16_t recent_fv_len;
 
-static char *p_Serial1_out = Serial1_out_buf;
+static char* p_Serial1_out = Serial1_out_buf;
 
 void sml_output_results(uint16_t model, uint16_t classification)
 {
-    #if SML_PROFILER
+#if SML_PROFILER
     kb_print_model_cycles(model, Serial1_out_buf, recent_fv_cycles);
-    #else
+#else
     kb_print_model_result(model, classification, Serial1_out_buf, 1, recent_fv);
-    #endif
+#endif  // SML_PROFILER
 
+
+#if USE_BLE
+    BLEDevice central = BLE.central();
+    if (central.connected())
+#if SERIAL_DEBUG
+        Serial.println("Sending ClassificationNotifiaction");
+#endif
+    Send_Notification(model, classification, features, fv_length);
+#else   // USE_BLE
     Serial.print(Serial1_out_buf);
     Serial.println("");
     Serial.flush();
-#if USE_BLE
-if(central.connected())
-    Send_Notification(model, classification, features, fv_length);
-
 #endif  // USE_BLE
 }
 
@@ -264,12 +284,13 @@ int setup_audio()
     PDM.onReceive(onPDMdata);
     if (!PDM.begin(1, 16000))
     {
+#if SERIAL_DEBUG
         Serial.println("Failed to start PDM!");
+#endif
         while (1)
             ;
     }
     return 0;
-
 }
 
 uint8_t* getSampleBuffer() { return (uint8_t*) sampleBuffer; }
@@ -286,15 +307,19 @@ static void onPDMdata()
     samplesRead = bytesAvailable / 2;
 }
 
-#endif
+#endif  // ENABLE_AUDIO
 
 void setup()
 {
+#if SERIAL_DEBUG || !USE_BLE
     Serial.begin(SERIAL_BAUD_RATE);
-    delay(2000);
-    Serial.println("Setting up...");
-    kb_model_init();
+    while (!Serial)
+        ;
+#if SERIAL_DEBUG
+    Serial.println("Serial Connection initialized.");
+#endif  // SERIAL_DEBUG
     delay(1000);
+#endif  // SERIAL_DEBUG || !USE_BLE
 
 #if ENABLE_ACCEL || ENABLE_GYRO || ENABLE_MAG
     setup_imu();
@@ -307,25 +332,52 @@ void setup()
 #endif
 
 #if USE_BLE
+#if SERIAL_DEBUG
     Serial.println("Start ble setup");
+#endif
     setup_ble();
     delay(1000);
     BLE.advertise();
     disconnectedLight();
+#if SERIAL_DEBUG
     Serial.println("Advertising...");
+#endif
 
 #endif
+
+
+    kb_model_init();
+
 
     memset(sensorRawData, 0, num_sensors * sizeof(int16_t));
 }
 
 void loop()
 {
+    currentMs = millis();
+
 #if USE_BLE
-    central = BLE.central();
-    if (central.connected())
+    BLEDevice central = BLE.central();
+    if (central)
     {
-        connectedLight();
+        if (central.connected())
+        {
+            connectedLight();
+        }
+    }
+    else
+    {
+        // disconnectedLight();
+
+        if (currentMs - previousBLECheckMs >= 5000)
+        {
+#if SERIAL_DEBUG
+            Serial.print("For Data Capture, connect to BLE address: ");
+            Serial.println(BLE.address());
+            Serial.println("Waiting...");
+#endif  // SERIAL_DEBUG
+            previousBLECheckMs = currentMs;
+        }
     }
 #endif  // USE_BLE
     sensorRawIndex = 0;
@@ -339,7 +391,7 @@ void loop()
 #endif
 
 #if ENABLE_AUDIO
-    num_sensors = 1;
+        num_sensors = 1;
         if (samplesRead)
         {
             for (int i = 0; i < samplesRead; i++)
